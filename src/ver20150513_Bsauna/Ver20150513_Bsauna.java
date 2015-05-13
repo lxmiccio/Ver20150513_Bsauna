8c72fe47ca9cbfe346fa7de4f0b4059d4f4a3d04
Alex Miccio <miccio.alex@gmail.com>
2015-05-13 15:33:14 +0200

    Ver20150513_Bsauna implemented

---
+++ src/ver20150513_Bsauna/Ver20150513_Bsauna.java
@@ -0,0 +1,14 @@
+package ver20150513_Bsauna;
+
+import gestionepalestra.*;
+
+public class Ver20150513_Bsauna
+{
+	public static void main(String[] args)
+	{
+		Palestra palestra = new Palestra("Virgin");
+		AltaTemperatura altaTemperatura = new AltaTemperatura("01", 8);
+		MediaTemperatura bassaTemperatura = new MediaTemperatura("02", 5);
+		
+	}
+}
